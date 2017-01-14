10205a786d5878302fa305209226c2d531bdb4eb
Alexander Warren <arocks124@gmail.com>
2017-01-14 10:46:14 -0500

    Added gear subsystem ports back to portmap

--- src/org/usfirst/frc/team1155/robot/PortMap.java
+++ src/org/usfirst/frc/team1155/robot/PortMap.java
@@ -2,6 +2,11 @@ package org.usfirst.frc.team1155.robot;
 
 public class PortMap {
 	
+	public static final int GEAR_SOLENOID_FWD = 0;
+	public static final int GEAR_SOLENOID_REV = 0;
+	public static final int ULTRASONIC_PING = 0;
+	public static final int ULTRASONIC_ECHO = 0;
+	
 	public static final int FRONT_LEFT_TALON = 0;
 	public static final int FRONT_RIGHT_TALON = 1;
 	public static final int BACK_LEFT_TALON = 2;
