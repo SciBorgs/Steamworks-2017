77a5240d20f0b6257767714aef8d722cc5c36967
lebowl <lebowl@bxscience.edu>
2017-01-23 16:32:13 -0500

    style fixes in GearSubsystem

--- src/org/usfirst/frc/team1155/robot/subsystems/GearSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/GearSubsystem.java
@@ -14,8 +14,8 @@ public class GearSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-	public static DoubleSolenoid ds;
-	public static Ultrasonic us;
+	public static DoubleSolenoid doubleSolenoid;
+	public static Ultrasonic ultrasonic;
 	
 	//Min and max distances for the wall and the gear when it is in the robot 
 	public static final double FARTHEST_FROM_WALL = 0.0;
@@ -24,34 +24,27 @@ public class GearSubsystem extends Subsystem {
 	public static final double GEAR_FARTHEST = 0.0;
 	
 	public GearSubsystem(){
-    	ds = new DoubleSolenoid(PortMap.GEAR_SOLENOID_FWD, PortMap.GEAR_SOLENOID_REV);
-    	us = new Ultrasonic(PortMap.ULTRASONIC_PING, PortMap.ULTRASONIC_ECHO);//, Ultrasonic.Unit.kInches);
+		doubleSolenoid = new DoubleSolenoid(PortMap.GEAR_SOLENOID_FWD, PortMap.GEAR_SOLENOID_REV);
+		doubleSolenoid = new Ultrasonic(PortMap.ULTRASONIC_PING, PortMap.ULTRASONIC_ECHO);//, Ultrasonic.Unit.kInches);
 	}
 	
 	//Checks whether the robot is in a suitable distance from the wall
 	public boolean validDistFromWall(){
-		if(us.getRangeInches() <= FARTHEST_FROM_WALL && us.getRangeInches() >= CLOSEST_TO_WALL){
-			return true;
-		}
-		return false;
-	}
+		return (us.getRangeInches() <= FARTHEST_FROM_WALL && us.getRangeInches() >= CLOSEST_TO_WALL);
 	
 	//Uses ultrasonic to detect wether the gear is in the robot
 	public boolean hasGear(){
-		if(us.getRangeInches() <= GEAR_FARTHEST && us.getRangeInches() >= GEAR_CLOSEST){
-			return true;
-		}
-		return false;
+		return (us.getRangeInches() <= GEAR_FARTHEST && us.getRangeInches() >= GEAR_CLOSEST);
 	}
 	
 	//Opens solenoid
 	public void open(){
-		ds.set(DoubleSolenoid.Value.kForward);
+		doubleSolenoid.set(DoubleSolenoid.Value.kForward);
 	}
 	
 	//Closes solenoid
 	public void close(){
-		ds.set(DoubleSolenoid.Value.kForward);
+		doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
 	}
 	
     public void initDefaultCommand() {
