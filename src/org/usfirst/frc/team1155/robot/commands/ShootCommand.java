ab0848416dc8e7e9a2bd115a7fed2b4e1bc7fc4f
P250-SandDune <lib@bxscience.edu>
2017-01-28 10:07:42 -0500

    made wheel speed be based on rtrigger

--- src/org/usfirst/frc/team1155/robot/commands/ShootCommand.java
+++ src/org/usfirst/frc/team1155/robot/commands/ShootCommand.java
@@ -29,7 +29,7 @@ public class ShootCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.shootSubsystem.setWheelSpeed(1);
+    	Robot.shootSubsystem.setWheelSpeed(OI.gamepad.getTriggerAxis(GenericHID.Hand.kRight));
     }
 
     // Make this return true when this Command no longer needs to run execute()
